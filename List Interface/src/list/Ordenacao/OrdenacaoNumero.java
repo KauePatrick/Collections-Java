90f6d2033e861c73238dc440e91e27511f3c826e
kaue_ <kauepatrick125@gmail.com>
2024-08-01 21:21:37 -0300

    Ordenação numeros

--- List Interface/src/list/Ordenacao/OrdenacaoNumero.java
+++ List Interface/src/list/Ordenacao/OrdenacaoNumero.java
@@ -1,4 +1,71 @@
 package list.Ordenacao;
 
-public class OrdenacaoNumero {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoNumero{
+
+    private List<Integer> numeroList;
+
+    public OrdenacaoNumero(){
+        this.numeroList = new ArrayList<>();
+    }
+
+    public void adicionarNumero(int numero){
+        this.numeroList.add(numero);
+    }
+
+    public List<Integer> ordenarAscendente(){
+        List<Integer> numeroAsc = new ArrayList<>(this.numeroList);
+       if(!numeroAsc.isEmpty()){
+           Collections.sort(numeroAsc);
+           return numeroAsc;
+       } else{
+           throw new RuntimeException("A lista está vazia!");
+       }
+
+    }
+
+    public List<Integer> ordenarDescendentes(){
+        List<Integer> numeroAsc = new ArrayList<>(this.numeroList);
+        if(!numeroList.isEmpty()){
+            numeroAsc.sort(Collections.reverseOrder());
+            return numeroAsc;
+        } else{
+            throw new RuntimeException("A lista está vazia!");
+        }
+    }
+
+    public void exibirNumeros(){
+        if(!numeroList.isEmpty()){
+            System.out.println(this.numeroList);
+        } else {
+            System.out.println("A lista está vazia!");
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+
+        OrdenacaoNumero numeros = new OrdenacaoNumero();
+
+        numeros.adicionarNumero(2);
+        numeros.adicionarNumero(5);
+        numeros.adicionarNumero(4);
+        numeros.adicionarNumero(1);
+        numeros.adicionarNumero(99);
+
+        numeros.exibirNumeros();
+
+        System.out.println(numeros.ordenarAscendente());
+
+        numeros.exibirNumeros();
+
+        System.out.println(numeros.ordenarDescendentes());
+
+        numeros.exibirNumeros();
+
+    }
 }
